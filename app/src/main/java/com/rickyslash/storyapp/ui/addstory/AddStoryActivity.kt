a05a208df0be0d4c314bdc22e11994e60b359723
ricky-kiva <ricky.kiva@gmail.com>
2023-05-03 01:15:32 +0700

    add CameraX Activity

--- app/src/main/java/com/rickyslash/storyapp/ui/addstory/AddStoryActivity.kt
+++ app/src/main/java/com/rickyslash/storyapp/ui/addstory/AddStoryActivity.kt
@@ -2,7 +2,10 @@ package com.rickyslash.storyapp.ui.addstory
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.content.Intent
+import android.graphics.BitmapFactory
 import android.graphics.drawable.ColorDrawable
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Spannable
@@ -10,6 +13,8 @@ import android.text.SpannableString
 import android.text.style.ForegroundColorSpan
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.view.menu.MenuBuilder
 import androidx.core.content.ContextCompat
 import androidx.datastore.core.DataStore
@@ -19,7 +24,9 @@ import androidx.lifecycle.ViewModelProvider
 import com.rickyslash.storyapp.R
 import com.rickyslash.storyapp.databinding.ActivityAddStoryBinding
 import com.rickyslash.storyapp.helper.ViewModelFactory
+import com.rickyslash.storyapp.helper.rotateAndCompressFile
 import com.rickyslash.storyapp.model.UserPreference
+import java.io.File
 
 private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "settings")
 
@@ -30,19 +37,42 @@ class AddStoryActivity : AppCompatActivity() {
         ActivityAddStoryBinding.inflate(layoutInflater)
     }
 
+    private var getFile: File? = null
+
+    private val launcherIntentCameraX = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+        if (it.resultCode == CAMERA_X_RESULT) {
+            val myFile = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                it.data?.getSerializableExtra("picture", File::class.java)
+            } else {
+                @Suppress("DEPRECATION")
+                it.data?.getSerializableExtra("picture")
+            } as? File
+
+            val isBackCamera = it.data?.getBooleanExtra("isBackCamera", true) as Boolean
+            myFile?.let { file ->
+                rotateAndCompressFile(file, isBackCamera)
+                getFile = file
+                binding.ivPreviewImg.setImageBitmap(BitmapFactory.decodeFile(file.path))
+                binding.tvPreviewImgText.visibility = View.GONE
+                binding.stAddFlPlaceholder.setOnClickListener(null)
+            }
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
         setupView()
         setupViewModel()
+        setupAction()
     }
 
     private fun setupView() {
         supportActionBar?.apply {
             val text = SpannableString(supportActionBar?.title)
             text.setSpan(ForegroundColorSpan(ContextCompat.getColor(this@AddStoryActivity, R.color.black)), 0, text.length, Spannable.SPAN_INCLUSIVE_INCLUSIVE)
-            elevation = "0".toFloat()
+            elevation = 0f
             setBackgroundDrawable(ColorDrawable(ContextCompat.getColor(this@AddStoryActivity, R.color.teal_EDC)))
             title = text
             setDisplayHomeAsUpEnabled(true)
@@ -52,6 +82,16 @@ class AddStoryActivity : AppCompatActivity() {
 
     private fun setupViewModel() {
         addStoryViewModel = ViewModelProvider(this, ViewModelFactory(UserPreference.getInstance(dataStore)))[AddStoryViewModel::class.java]
+    }
+
+    private fun setupAction() {
+        binding.ibCamera.setOnClickListener { startCameraX() }
+        binding.stAddFlPlaceholder.setOnClickListener { startCameraX() }
+    }
+
+    private fun startCameraX() {
+        val intent = Intent(this, CameraActivity::class.java)
+        launcherIntentCameraX.launch(intent)
     }
 
     @SuppressLint("RestrictedApi")
@@ -76,5 +116,9 @@ class AddStoryActivity : AppCompatActivity() {
             }
             else -> true
         }
+    }
+
+    companion object {
+        const val CAMERA_X_RESULT = 200
     }
 }
---
+++ app/src/main/java/com/rickyslash/storyapp/ui/addstory/CameraActivity.kt
@@ -0,0 +1,126 @@
+package com.rickyslash.storyapp.ui.addstory
+
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowManager
+import android.widget.Toast
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
+import android.Manifest
+import androidx.core.app.ActivityCompat
+import com.rickyslash.storyapp.databinding.ActivityCameraBinding
+import com.rickyslash.storyapp.helper.createFile
+
+class CameraActivity : AppCompatActivity() {
+
+    private val binding by lazy(LazyThreadSafetyMode.NONE) {
+        ActivityCameraBinding.inflate(layoutInflater)
+    }
+
+    private var imageCapture: ImageCapture? = null
+    private var cameraSelector: CameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        if (!allPermissionGranted()) {
+            ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
+        }
+
+        binding.ibCaptureCam.setOnClickListener { takePhoto() }
+        binding.ivSwitchCam.setOnClickListener {
+            cameraSelector = if (cameraSelector == CameraSelector.DEFAULT_BACK_CAMERA) CameraSelector.DEFAULT_FRONT_CAMERA
+            else CameraSelector.DEFAULT_BACK_CAMERA
+
+            startCamera()
+        }
+    }
+
+    public override fun onResume() {
+        super.onResume()
+        hideSystemUI()
+        startCamera()
+    }
+
+    private fun startCamera() {
+        val cameraProvideFuture = ProcessCameraProvider.getInstance(this)
+        cameraProvideFuture.addListener({
+            val cameraProvider: ProcessCameraProvider = cameraProvideFuture.get()
+            val preview = Preview.Builder()
+                .build()
+                .also { it.setSurfaceProvider(binding.pvPreviewCam.surfaceProvider) }
+
+            imageCapture = ImageCapture.Builder().build()
+
+            try {
+                cameraProvider.unbindAll()
+                cameraProvider.bindToLifecycle(this, cameraSelector, preview, imageCapture)
+            } catch (e: Exception) {
+                Toast.makeText(this@CameraActivity, "Failed to launch camera", Toast.LENGTH_SHORT).show()
+            }
+        }, ContextCompat.getMainExecutor(this))
+    }
+
+    private fun takePhoto() {
+        val imageCapture = imageCapture ?: return
+        val photoFile = createFile(application)
+        val outputOptions = ImageCapture.OutputFileOptions.Builder(photoFile).build()
+
+        imageCapture.takePicture(
+            outputOptions,
+            ContextCompat.getMainExecutor(this),
+            object : ImageCapture.OnImageSavedCallback {
+                override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
+                    Toast.makeText(this@CameraActivity, "Image captured!", Toast.LENGTH_SHORT).show()
+                    val intent = Intent()
+                    intent.putExtra("picture", photoFile)
+                    intent.putExtra("isBackCamera", cameraSelector == CameraSelector.DEFAULT_BACK_CAMERA)
+                    setResult(AddStoryActivity.CAMERA_X_RESULT, intent)
+                    finish()
+                }
+
+                override fun onError(exception: ImageCaptureException) {
+                    Toast.makeText(this@CameraActivity, "Failed to capture image", Toast.LENGTH_SHORT).show()
+                }
+            }
+        )
+    }
+
+    private fun allPermissionGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(baseContext, it) == PackageManager.PERMISSION_GRANTED
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (!allPermissionGranted()) {
+                Toast.makeText(this, "Insufficient permission", Toast.LENGTH_SHORT).show()
+                finish()
+            }
+        }
+    }
+
+    private fun hideSystemUI() {
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        }
+        supportActionBar?.hide()
+    }
+
+    companion object {
+        private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+        private const val REQUEST_CODE_PERMISSIONS = 10
+    }
+}
