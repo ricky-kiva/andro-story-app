73b7607b918daf193814923b2c41b74995c2de85
ricky-kiva <ricky.kiva@gmail.com>
2023-05-03 13:28:21 +0700

    complete AddStory Activity

--- app/src/main/java/com/rickyslash/storyapp/ui/addstory/AddStoryActivity.kt
+++ app/src/main/java/com/rickyslash/storyapp/ui/addstory/AddStoryActivity.kt
@@ -3,8 +3,10 @@ package com.rickyslash.storyapp.ui.addstory
 import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
+import android.content.Intent.ACTION_GET_CONTENT
 import android.graphics.BitmapFactory
 import android.graphics.drawable.ColorDrawable
+import android.net.Uri
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -14,18 +16,29 @@ import android.text.style.ForegroundColorSpan
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
+import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.view.menu.MenuBuilder
 import androidx.core.content.ContextCompat
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.preferencesDataStore
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.rickyslash.storyapp.R
 import com.rickyslash.storyapp.databinding.ActivityAddStoryBinding
-import com.rickyslash.storyapp.helper.ViewModelFactory
-import com.rickyslash.storyapp.helper.rotateAndCompressFile
+import com.rickyslash.storyapp.helper.*
 import com.rickyslash.storyapp.model.UserPreference
+import com.rickyslash.storyapp.ui.login.LoginActivity
+import com.rickyslash.storyapp.ui.main.MainActivity
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
+import okhttp3.RequestBody.Companion.asRequestBody
+import okhttp3.RequestBody.Companion.toRequestBody
 import java.io.File
 
 private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "settings")
@@ -37,7 +50,11 @@ class AddStoryActivity : AppCompatActivity() {
         ActivityAddStoryBinding.inflate(layoutInflater)
     }
 
-    private var getFile: File? = null
+    private var isErrorObserver: Observer<Boolean>? = null
+    private var responseMessageObserver: Observer<String?>? = null
+    private var isLoadingObserver: Observer<Boolean>? = null
+
+    private var fileToUpload: File? = null
 
     private val launcherIntentCameraX = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
         if (it.resultCode == CAMERA_X_RESULT) {
@@ -50,11 +67,21 @@ class AddStoryActivity : AppCompatActivity() {
 
             val isBackCamera = it.data?.getBooleanExtra("isBackCamera", true) as Boolean
             myFile?.let { file ->
-                rotateAndCompressFile(file, isBackCamera)
-                getFile = file
+                rotateImageFile(file, isBackCamera)
+                fileToUpload = file
                 binding.ivPreviewImg.setImageBitmap(BitmapFactory.decodeFile(file.path))
-                binding.tvPreviewImgText.visibility = View.GONE
-                binding.stAddFlPlaceholder.setOnClickListener(null)
+                imageContainerFilled()
+            }
+        }
+    }
+
+    private val launcherIntentGallery = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+        if (it.resultCode == RESULT_OK) {
+            val selectedImg = it.data?.data as Uri
+            selectedImg.let { uri ->
+                fileToUpload = uriToFile(uri, this@AddStoryActivity)
+                binding.ivPreviewImg.setImageURI(uri)
+                imageContainerFilled()
             }
         }
     }
@@ -82,16 +109,111 @@ class AddStoryActivity : AppCompatActivity() {
 
     private fun setupViewModel() {
         addStoryViewModel = ViewModelProvider(this, ViewModelFactory(UserPreference.getInstance(dataStore)))[AddStoryViewModel::class.java]
+        observeLoading()
+        addStoryViewModel.getUser().observe(this) { user ->
+            if (user.isLogin) {
+                binding.tvGreetName.text = getString(R.string.greet_name, titleSentence(user.name))
+                binding.ibUpload.setOnClickListener { uploadStory(user.token) }
+            } else {
+                val intent = Intent(this@AddStoryActivity, LoginActivity::class.java)
+                intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+                startActivity(intent)
+                finish()
+            }
+        }
     }
 
     private fun setupAction() {
-        binding.ibCamera.setOnClickListener { startCameraX() }
         binding.stAddFlPlaceholder.setOnClickListener { startCameraX() }
+        binding.ibCamera.setOnClickListener { startCameraX() }
+        binding.ibGallery.setOnClickListener { startGallery() }
     }
 
     private fun startCameraX() {
         val intent = Intent(this, CameraActivity::class.java)
         launcherIntentCameraX.launch(intent)
+    }
+
+    private fun startGallery() {
+        val intent = Intent()
+        intent.action = ACTION_GET_CONTENT
+        intent.type = "image/*"
+
+        val chooser = Intent.createChooser(intent, "Choose a Picture")
+        launcherIntentGallery.launch(chooser)
+    }
+
+    private fun imageContainerFilled() {
+        binding.tvPreviewImgText.visibility = View.GONE
+        binding.stAddFlPlaceholder.setOnClickListener(null)
+    }
+
+    private fun uploadStory(token: String) {
+        if (fileToUpload != null) {
+            if (binding.edtxDesc.text?.isNotBlank() == true) {
+                val file = reduceFileImage(fileToUpload as File)
+                val desc = binding.edtxDesc.text.toString().toRequestBody("text/plain".toMediaType())
+                val requestImageFile = file.asRequestBody("image/jpeg".toMediaType())
+                val imageMultipart: MultipartBody.Part = MultipartBody.Part.createFormData("photo", file.name, requestImageFile)
+                dialogOnUpload(token, imageMultipart, desc)
+            } else {
+                Toast.makeText(this@AddStoryActivity, "Please add a description", Toast.LENGTH_SHORT).show()
+            }
+        } else {
+            Toast.makeText(this@AddStoryActivity, "Please add an image", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun processUploadStory(token: String, file: MultipartBody.Part, desc: RequestBody) {
+        addStoryViewModel.uploadStory(token, file, desc)
+        isErrorObserver = Observer { isError ->
+            if (!isError) {
+                Toast.makeText(this, "Posted! \uD83C\uDF89", Toast.LENGTH_SHORT).show()
+                val intent = Intent(this@AddStoryActivity, MainActivity::class.java)
+                intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+                startActivity(intent)
+                finish()
+            } else {
+                responseMessageObserver = Observer { responseMessage ->
+                    if (responseMessage != null) {
+                        Toast.makeText(this, responseMessage, Toast.LENGTH_SHORT).show()
+                    }
+                }
+                responseMessageObserver?.let {
+                    addStoryViewModel.responseMessage.observeOnce(this, it)
+                }
+            }
+        }
+        isErrorObserver?.let {
+            addStoryViewModel.isError.observe(this, it)
+        }
+    }
+
+    private fun dialogOnUpload(token: String, file: MultipartBody.Part, desc: RequestBody) {
+        AlertDialog.Builder(this).apply {
+            setTitle("\uD83D\uDE80 Story Launch")
+            setMessage("Ready to drop your epic story to the world?")
+            setPositiveButton("Yes") { _, _ ->
+                processUploadStory(token, file, desc)
+            }
+            setNegativeButton("No") { dialog, _ ->
+                dialog.dismiss()
+            }
+            create()
+            show()
+        }
+    }
+
+    private fun observeLoading() {
+        isLoadingObserver = Observer { showLoading(it) }
+        isLoadingObserver?.let {
+            addStoryViewModel.isLoading.observe(this, it)
+        }
+    }
+
+    private fun showLoading(isLoading: Boolean) {
+        binding.progressBar.visibility = if (isLoading) View.VISIBLE else View.GONE
+        binding.ibUpload.visibility = if (isLoading) View.GONE else View.VISIBLE
     }
 
     @SuppressLint("RestrictedApi")
@@ -116,6 +238,22 @@ class AddStoryActivity : AppCompatActivity() {
             }
             else -> true
         }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        isErrorObserver?.let(addStoryViewModel.isError::removeObserver)
+        responseMessageObserver?.let(addStoryViewModel.responseMessage::removeObserver)
+        isLoadingObserver?.let(addStoryViewModel.isLoading::removeObserver)
+    }
+
+    private fun <T> LiveData<T>.observeOnce(owner: LifecycleOwner, observer: Observer<T>) {
+        observe(owner, object : Observer<T> {
+            override fun onChanged(value: T) {
+                observer.onChanged(value)
+                removeObserver(this)
+            }
+        })
     }
 
     companion object {
--- app/src/main/java/com/rickyslash/storyapp/ui/addstory/AddStoryViewModel.kt
+++ app/src/main/java/com/rickyslash/storyapp/ui/addstory/AddStoryViewModel.kt
@@ -1,16 +1,80 @@
 package com.rickyslash.storyapp.ui.addstory
 
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
+import android.util.Log
+import androidx.lifecycle.*
+import com.rickyslash.storyapp.api.ApiConfig
+import com.rickyslash.storyapp.api.response.AddStoryResponse
+import com.rickyslash.storyapp.api.response.AllStoriesResponse
+import com.rickyslash.storyapp.model.UserModel
 import com.rickyslash.storyapp.model.UserPreference
+import com.rickyslash.storyapp.ui.login.LoginViewModel
+import com.rickyslash.storyapp.ui.main.MainViewModel
 import kotlinx.coroutines.launch
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.http.Multipart
 
 class AddStoryViewModel(private val pref: UserPreference): ViewModel() {
+
+    private val _isLoading = MutableLiveData<Boolean>()
+    val isLoading: LiveData<Boolean> = _isLoading
+
+    private val _isError = MutableLiveData<Boolean>()
+    val isError: LiveData<Boolean> = _isError
+
+    private val _responseMessage = MutableLiveData<String?>()
+    val responseMessage: LiveData<String?> = _responseMessage
+
+    fun getUser(): LiveData<UserModel> {
+        return pref.getUser().asLiveData()
+    }
 
     fun logout() {
         viewModelScope.launch {
             pref.logout()
         }
+    }
+
+    fun uploadStory(token: String, file: MultipartBody.Part, desc: RequestBody) {
+        _isLoading.value = true
+        val client = ApiConfig.getApiService(token).uploadStory(file, desc)
+        client.enqueue(object : Callback<AddStoryResponse> {
+            override fun onResponse(
+                call: Call<AddStoryResponse>,
+                response: Response<AddStoryResponse>
+            ) {
+                if (response.isSuccessful) {
+                    _isLoading.value = false
+                    val responseBody = response.body()
+                    if (responseBody != null) {
+                        _isError.value = responseBody.error
+                        _responseMessage.value = responseBody.message
+                        _responseMessage.value = null
+                    }
+                } else {
+                    _isLoading.value = false
+                    _isError.value = true
+                    _responseMessage.value = response.message()
+                    Log.e(AddStoryViewModel.TAG, "isNotSuccessful: ${response.message()}")
+                    _responseMessage.value = null
+                }
+            }
+
+            override fun onFailure(call: Call<AddStoryResponse>, t: Throwable) {
+                _isLoading.value = false
+                _isError.value = true
+                _responseMessage.value = t.message
+                Log.e(AddStoryViewModel.TAG, "onFailure: ${t.message}")
+                _responseMessage.value = null
+            }
+        })
+    }
+
+    companion object {
+        private val TAG = AddStoryViewModel::class.java.simpleName
     }
 
 }
